eb30c07e85913770b9b8b24f5a3289010cdf1f82
0TH0N <jbator777@gmail.com>
2019-04-23 12:38:03 +0300

    First commit.

---
+++ src/entry.js
@@ -0,0 +1,8 @@
+import './js/index.jsx';
+import './js/App.jsx';
+import './sass/main.sass';
+import './sass/_vars.sass';
+import './sass/_fonts.sass';
+import './sass/_libs.sass';
+//import './sass/_media.sass';
+import './sass/_mixins/_font-face.sass';
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './entry.js',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'css/style.css',
+    }),
+    new HtmlWebpackPlugin({template: './index.html'}),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:8080/'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        
+        test: /\.(sa|sc|c)ss$/,
+        exclude: [
+          /node_modules/,
+        ],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      }
+    ]
+  }
+};
